21b9ada52cdbe21b92465b554e86910da5a90720
ghassen ben othmen <gbenothmen2@gmail.com>
2021-12-31 12:41:12 +0100

    feat: Adding Models

---
+++ src/Models/Attachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Models.Utils.Utility;
+
+namespace Models
+{
+    public class Attachment
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal Size { get; set; }
+        public AttachmentType Type { get; set; }
+    }
+}
---
+++ src/Models/Utils/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Utils
+{
+    public static class Utility
+    {
+        public enum AttachmentType
+        {
+            PDF,
+            PNG,
+            JPEG,
+            JPG,
+            GIF,
+            DOC,
+            DOCX,
+            XLS,
+            XLSX,
+            PPT,
+            PPTX,
+            TXT,
+
+        }
+    }
+}
